e2dda73d56c57a47b87ec1f0ab75ff4844ac0e64
eric7string <153536554+eric7string@users.noreply.github.com>
2024-09-21 10:37:09 -0400

    created add role function, get departments function, and added line breaks for terminal formatting

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,13 @@ await connectToDb();
 
 interface DepartmentInput{
   name: string;
-}
+};
+
+interface RoleInput{
+  title: string;
+  salary: number;
+  department_id: number;
+};
 
 const PORT = 3001;
 const app = express();
@@ -25,7 +31,9 @@ const viewAllDepartments = async () => {
       console.error('Error executing query', error);
       return;
     }
+    console.log('\n---Departments---\n');
     console.table(result.rows);
+    console.log('\n' + '-'.repeat(50) + '\n');
   });
 };
 
@@ -36,20 +44,26 @@ const viewAllRoles = async () => {
     if (error) {
       console.error('Error executing query', error);
       return;
+    
     }
+    console.log('\n---Departments---\n');
     console.table(result.rows);
+    console.log('\n' + '-'.repeat(50) + '\n')
+   
   });
 };
 
 // view all employees
 
 const viewAllEmployees = async () => {
-  pool.query('SELECT * FROM department', (error: Error, result: QueryResult) => {
+  pool.query('SELECT * FROM employee', (error: Error, result: QueryResult) => {
     if (error) {
       console.error('Error executing query', error);
       return;
     }
+    console.log('\n---Departments---\n');
     console.table(result.rows);
+    console.log('\n' + '-'.repeat(50) + '\n')
   });
 }
 //add a department
@@ -65,11 +79,6 @@ const addDepartment = async (department: DepartmentInput): Promise<void> => {
     console.error('Error executing query', error);
   }
 };
-//add a role
-
-//add an employee
-
-//update an employee role
 const addDeptQuery = async (): Promise<void> => {
  const answers = await inquirer.prompt<DepartmentInput>([
     {
@@ -80,46 +89,110 @@ const addDeptQuery = async (): Promise<void> => {
   ]);
   const { name } = answers;
   await addDepartment({ name });
+};
+// get departments
+const getDepartments = async (): Promise<{ id: number; name: string }[]> => {
+  return new Promise((resolve, reject) => {
+    pool.query('SELECT id, name FROM department', (error: Error, result: QueryResult) => {
+      if (error) {
+        reject(error);
+        return;
+      }
+      resolve(result.rows);
+
+    });
+  });
+};
+
+//add a role
+const addRole = async (role: RoleInput): Promise<void> => {
+  const query = {
+    text: 'INSERT INTO role (title, salary, department_id) VALUES ($1, $2, $3)',
+    values: [role.title, role.salary, role.department_id],
+  };
+  try {
+    await pool.query(query);
+    console.log('Role added successfully');
+  } catch (error) {
+    console.error('Error executing query', error);
+  }
+};
+
+const addRoleQuery = async (): Promise<void> => {
+  try {
+    const departments = await getDepartments();
+    const departmentChoices = departments.map((department) => ({
+      name: department.name,
+      value: department.id,
+    }));
+
+  const answers = await inquirer.prompt<RoleInput>([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'Enter the title of the role',
+    },
+    {
+      type: 'number',
+      name: 'salary',
+      message: 'Enter the salary of the role',
+    },
+    {
+      type: 'list',
+      name: 'department_id',
+      message: 'Select the department for the role',
+      choices: departmentChoices,
+    },
+  ]);
+  const { title, salary, department_id } = answers;
+  await addRole({ title, salary, department_id });
+} catch (error) {
+  console.error('Error adding role', error);
+} ;
 }
 
+
+//add an employee
+
+//update an employee role
 
 
 
 
 // inquirer
-inquirer
-  .prompt([
-    {
-      type: 'list',
-      name: 'action',
-      message: 'What would you like to do?',
-      choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department' ], //'Add a department', 'Add a role', 'Add an employee', 'Update an employee role'
-    },
-  ])
-  .then((answers) => {
-    if (answers.action === 'View all departments') {
-        viewAllDepartments();
-        return;
-    } else if (answers.action === 'View all roles') {
-        viewAllRoles();
-        return;
-    } else if (answers.action === 'View all employees') {
-        viewAllEmployees();
-        return;
-    } else if (answers.action === 'Add a department') {
-      addDeptQuery();
-      return;
-    }
-  });
-
-
-
-
+async function mainMenu() {
+    const answers= await inquirer.prompt([
+        {
+          type: 'list',
+          name: 'action',
+          message: 'What would you like to do?',
+          choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', "Add a role", ], //'Add a department', 'Add a role', 'Add an employee', 'Update an employee role'
+        },
+      ])
+      
+        if (answers.action === 'View all departments') {
+           await viewAllDepartments();
+        } else if (answers.action === 'View all roles') {
+          await  viewAllRoles();
+        } else if (answers.action === 'View all employees') {
+          await  viewAllEmployees();
+        } else if (answers.action === 'Add a department') {
+         await addDeptQuery();
+        } else if (answers.action === 'Add a role') {
+          await addRoleQuery();
+        }
+        console.clear();
+        await mainMenu();
+      };
+      
+      
+      
+      mainMenu();
 
 
 
 
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`\n Server running on port ${PORT}\n`);
+}); 
